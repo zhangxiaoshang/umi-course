4d07b39ea31ea260bd757017b28307d138822d08
zhangxiaoshang <zhangxiaoshang66@163.com>
2020-04-24 19:11:14 +0800

    dva models

---
+++ src/pages/hero.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import styles from './hero.less';
+import { connect, HeroModelState, ConnectProps } from 'umi';
+// import { HeroModelState } from '@/models/hero'
+
+interface PageProps extends ConnectProps {
+  hero: HeroModelState;
+}
+
+const Hero: FC<PageProps> = props => {
+  console.log(props.hero);
+  return (
+    <div>
+      <h1 className="styles.title">Page hero</h1>
+      <h2>This is {props.hero.name}</h2>
+    </div>
+  );
+};
+
+export default connect(({ hero }: { hero: HeroModelState }) => ({ hero }))(
+  Hero,
+);
